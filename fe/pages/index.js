dacd05029a943536eedfa7261fbb62db2db2b99c
matanbe20 <matanbe18@gmail.com>
2019-05-10 12:34:49 +0300

    rearranged project with nextjs and srr styles

---
+++ fe/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+const index = () => {
+  return (
+    <div>
+     <Button variant="contained" onClick={() => confirm('do you love me do u do du')}>Press Me!</Button>
+    </div>
+  )
+}
+
+export default index
